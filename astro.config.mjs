8f64ccf13d747f765e40affe609d4f1ad438d1da
smolina97 <santi.lds17@gmail.com>
2024-04-18 12:45:38 -0500

    Update site and base configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://smolina97.github.io',
+    site: 'https://smolina97.github.io/portfolio-min-astro/',
     base: 'portfolio-min-astro',
 });
