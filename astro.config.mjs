59e8c8cacd3e85549a2404433d72293066a3cc9f
smolina97 <santi.lds17@gmail.com>
2024-04-18 14:17:07 -0500

    mejor usar vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://smolina97.github.io',
-    base: 'portfolio-min-astro',
 });
