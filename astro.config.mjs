6095a269cca5b1438258484c09eb188665a74478
smolina97 <santi.lds17@gmail.com>
2024-04-18 12:34:18 -0500

    Add site and base configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://smolina97.github.io',
+    base: 'portfolio-min-astro',
+});
